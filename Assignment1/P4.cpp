682593c8057df321a50d034afa64f41b740e888f
MIN <104954505+wallradon@users.noreply.github.com>
2024-07-31 21:06:09 +0700

    Update

--- Assignment1/P4.cpp
+++ Assignment1/P4.cpp
@@ -1,4 +1,9 @@
 #include <stdio.h>
-int main(){
-    
-}
+int main( ) {
+    int a = 3 ;
+    int b = 4 ;
+    if( a > b ) {
+        printf( "The number in A is greater than B. \n" ) ;
+    }//eng if
+    printf( "\n Hello World." ) ;
+}//eng function
