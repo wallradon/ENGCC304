bab0e31cc03b272ae18bf42a3e592eadb1b81410
MIN <104954505+wallradon@users.noreply.github.com>
2024-07-31 21:14:43 +0700

    Add files via upload

---
+++ Assignment1/P6.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a = 3 ;
+    int b = 4 ;
+    if( a > b ) {
+        printf( "The number in A is greater than B. \n" ) ;
+    } 
+    else {
+        printf( "The number in B is greater than A. \n" ) ;
+    }//end if
+}//end function
