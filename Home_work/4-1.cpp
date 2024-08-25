1c49a07e7aa7b631991a94cbabfa568c330c9f6c
MIN <104954505+wallradon@users.noreply.github.com>
2024-08-25 15:19:20 +0700

    Create 4-1.cpp

---
+++ Home_work/4-1.cpp
@@ -0,0 +1,15 @@
+/*
+    จงแสดงผลตัวแปรทั้งหมดเรียงลำดับดังนี้ a b c d
+
+    Output:
+        10 20.0 Haha a
+*/
+
+#include <stdio.h>
+int main(){
+    int a = 10 ;
+    float b = 20 ;
+    char c[] = "Haha" ;
+    char d = 'a' ;
+    printf( "%d, %0.1f, %s, %c", a, b, c, d ) ; 
+}//endfuntion
