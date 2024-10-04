24acfef8c0896826589c556ae8c3a99c23ea643d
MIN <104954505+wallradon@users.noreply.github.com>
2024-10-04 14:47:03 +0700

    Update LAB1.cpp

--- LAB1.cpp
+++ LAB1.cpp
@@ -1,13 +1,13 @@
-#include <stduio.h>
+#include <stdio.h>
 
 int main() {
     char Name[50] ;
     int  Age = 0 ;
-    printf( "Enter your name: " ) 
+    printf( "Enter your name: " ) ;
     scanf( "%s", Name ) ;
     printf( "Enter your age: " ) ;
-    scanf( "%d", Age ) ;
-    print( "- - - - - -\n" ) ;
+    scanf( "%d", &Age ) ;
+    printf( "- - - - - -\n" ) ;
     printf( "Hello %s \n", Name ) ; //TODO: #14 Say hello to user.
-    printf( "Age = %d\n", &Age ) ; //TODO: #15 display user their age number plus 1.
+    printf( "Age = %d\n", Age + 1 ) ; //TODO: #15 display user their age number plus 1.
 }//end main function
